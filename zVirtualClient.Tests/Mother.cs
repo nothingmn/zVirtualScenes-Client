b88d1da66a20153eb11028f8d2c173d047fe4de7
Rob Chartier <rob.chartier@gmail.com>
2012-07-22 16:47:03 -0700

    added missed mother class

---
+++ zVirtualClient.Tests/Mother.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zVirtualClient.Tests
+{
+    public class Mother
+    {
+        public static Credentials Credentials
+        {
+            get
+            {
+                return new Credentials("http://styx", 8008, null, "password");
+            }
+        }
+    }
+}
